d54f259bbc31029d73d00f528017c092093bc6de
LauYann <metalyanni@gmail.com>
2023-06-02 12:52:50 -0600

    Agregando funcion basica al presionar una opcion

--- GamePaper/GamePaper/ContentView.swift
+++ GamePaper/GamePaper/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     let moves = ["Rock","Paper","Scissors"]
+    @State private var showingAlert = false
     
     var body: some View {
         ZStack{
@@ -32,13 +33,19 @@ struct ContentView: View {
                             Button(action: {
                                 //TODO: Accion a realizar cuando el usuario presione el boton
                                 
+                                showingAlert.toggle()
                             }){
                                 Image("\(self.moves[moveId])")
                                     .resizable()
                                     .frame(width: 80,height: 100)
                             }
                             .frame(width: 100,height: 100,alignment: .center)
-                            
+                            .alert(isPresented: self.$showingAlert){
+                                //TODO: Presentara el score del usuario y sabra si gano o perdio tras X intentos
+                                Alert(title: Text("Hello"),
+                                      message: Text("Presionaste: \(moves[moveId])"),
+                                      dismissButton: .default(Text("Ok")))
+                            }
                             
                         }
                     }
